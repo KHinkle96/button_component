a6f990890946290f12972ded081845f15be65d07
khinkle96 <khinkle0512@gmail.com>
2020-03-06 21:56:03 -0800

    Created Button constant using props by specific names that are children, type, onClick, buttonStyle, and buttonSize. In the return I put the button onclick value to the onclick prop, type value to the type prop

---
+++ Button.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import "./button.css"
+
+const Button = ({
+    children,
+    type,
+    onClick,
+    buttonStyle,
+    buttonSize
+}) => {
+
+    return (
+        <button onClick={onClick} type={type}>
+            {children}
+        </button>
+    )
+}
+
+export default Button
