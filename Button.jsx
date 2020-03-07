8364627402f032348925172f1086e1b1f03826f6
khinkle96 <khinkle0512@gmail.com>
2020-03-06 22:02:47 -0800

    created arrays for the styles of buttons and the sizes

--- Button.jsx
+++ Button.jsx
@@ -1,6 +1,18 @@
 import React from "react"
 import "./button.css"
 
+const Styles = [
+    "btn-primary",
+    "btn-warning",
+    "btn-danger",
+    "btn-success",
+]
+
+const Sizes = [
+    "btn-small",
+    "btn-medum"
+]
+
 const Button = ({
     children,
     type,
